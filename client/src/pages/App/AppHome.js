4f8ebbf6a294426b0f7200dcfba6f5c1c19785d6
fThAbhishek-Pandey <abhishek830564@email.com>
2024-11-15 11:00:39 +0530

    client setup completed

---
+++ client/src/pages/App/AppHome.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+const AppHome = () => {
+  return (
+    <div>
+        <Routes>
+            <Route ></Route>
+        </Routes>
+
+    </div>
+  )
+}
+
+export default AppHome
